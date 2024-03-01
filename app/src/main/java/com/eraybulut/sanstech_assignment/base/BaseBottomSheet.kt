9ae7bb947f20002f4fee6fe87485f930f7d1891c
Eray <eraybulutlar@gmail.com>
2024-03-02 01:14:15 +0300

    Add BaseBottomSheet

---
+++ app/src/main/java/com/eraybulut/sanstech_assignment/base/BaseBottomSheet.kt
@@ -0,0 +1,40 @@
+package com.eraybulut.sanstech_assignment.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**
+ * Created by Eray BULUT on 2.03.2024
+ * eraybulutlar@gmail.com
+ */
+
+abstract class BaseBottomSheet<VB : ViewBinding>(
+    private val bindingInflater: (inflater: LayoutInflater) -> VB
+) : BottomSheetDialogFragment() {
+
+    private var _binding: VB? = null
+    protected val binding: VB get() = _binding as VB
+
+    protected abstract fun onCreateFinished()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater.invoke(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onCreateFinished()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
